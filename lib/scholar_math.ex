eea0d437de84014848b9bceff5c5de649621e3c9
Jorge Bejar <jorge@wyeworks.com>
2019-01-15 19:37:47 -0300

    Macro & caller context

--- lib/scholar_math.ex
+++ lib/scholar_math.ex
@@ -1,6 +1,10 @@
 defmodule ScholarMath do
   defmacro draw({:+, _meta, [lhs, rhs]}) do
+    IO.puts "In macro's context (#{__MODULE__})."
+
     quote do
+      IO.puts "In caller's context (#{__MODULE__})."
+
       IO.puts """
         #{unquote(lhs)}
       + #{unquote(rhs)}
